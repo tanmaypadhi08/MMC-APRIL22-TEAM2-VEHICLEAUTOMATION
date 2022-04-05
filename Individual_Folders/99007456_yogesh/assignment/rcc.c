be8eb10c46dda0d2e3e60f2a4c81f16cb921710d
Y-133 <98869524+Y-133@users.noreply.github.com>
2022-04-05 22:07:16 +0530

    Add files via upload

---
+++ Individual_Folders/99007456_yogesh/assignment/rcc.c
@@ -0,0 +1,13 @@
+# include "rcc.h"
+void config_rcc(void *ptr)
+{
+	if ((GPIORegDef *) ptr == GPIOD)
+	{
+		RCC->AHB1ENR = RCC->AHB1ENR | 0x8;
+	}
+
+	if ((GPIORegDef *) ptr == GPIOA)
+	{
+		RCC->AHB1ENR = RCC->AHB1ENR | 0x1;
+	}
+}
---
+++ Individual_Folders/99007456_yogesh/assignment/rcc.h
@@ -0,0 +1,6 @@
+#ifndef RCC_H
+#define RCC_H
+#include "gpio.h"
+void config_rcc(void *ptr);
+
+endif
